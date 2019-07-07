64cf858b08f015e850bd47ac1f4a28e26f575220
paktan <pakan@gmail.com>
2019-07-07 23:36:11 +0900

    jax add

--- src/App.js
+++ src/App.js
@@ -1,38 +1,49 @@
-import React from  'react';
+import React, {Component} from  'react';
 import logo from './logo.svg';
 import './App.css';
 import Movie from './Movie';
 
-const movies = [
-  {
-    title: "장원영",
-    poster: "https://upload.wikimedia.org/wikipedia/commons/a/a0/181006_%EC%95%84%EC%9D%B4%EC%A6%88%EC%9B%90_%EC%9E%A5%EC%9B%90%EC%98%81_%EB%AF%B8%EC%95%BC%EC%99%80%ED%82%A4_%EC%82%AC%EC%BF%A0%EB%9D%BC_%EC%9E%A0%EC%8B%A4_%EC%8B%9C%EA%B5%AC%EC%8B%9C%ED%83%80_03.jpg"
-  },
-  {
-    title: "미야와키 사쿠라",
-    poster: "https://upload.wikimedia.org/wikipedia/commons/5/53/181018_%EC%82%AC%EC%BF%A0%EB%9D%BC_02.png"
-  },
-  {
-    title: "조유리",
-    poster: "https://upload.wikimedia.org/wikipedia/commons/9/90/181029_IZ%2AONE_Yuri_02.png",
-  },
-  {
-    title: "안유진",
-    poster: "https://upload.wikimedia.org/wikipedia/commons/6/65/181029_IZ%2AONE_Yujin_02.png"
+
+class App extends Component {
+  state = {
   }
 
-]
+  componentDidMount(){
+    this._getMovies()
+  }
 
-function App() {
-  return (
-    <div className="App">
-      {
-        movies.map((movie, index) => {
-          return <Movie title={movie.title} poster={movie.poster} key={index}/>
-        })
-      }
-    </div>
-  );
+  _getMovies = async () => {
+    const movies = await this._callMovies()
+    console.log(movies)
+    this.setState({
+      movies
+    })
+  }
+
+  _callMovies = () => {
+    return fetch('https://yts.lt/api/v2/list_movies.json?sort_by=rating')
+    .then(patato => patato.json())
+    .then(json => json.data.movies)
+    .catch(err => console.log(err))
+  }
+
+  _renderMovie = () => {
+    const movies = this.state.movies.map(movie => {
+      return <Movie title={movie.title} poster={movie.large_cover_image} key={movie.id}/>
+    })
+
+    return movies
+  }
+
+  render(){
+    return (
+      <div className="App">
+        {
+          this.state.movies ? this._renderMovie() : "loading"
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Movie.js
+++ src/Movie.js
@@ -1,23 +1,28 @@
-import React, {Component} from  'react';
+import React from  'react';
+import PropTypes from 'prop-types';
 import './Movie.css';
 
-class Movie extends Component {
-    render() {
-        return (
-            <div>
-                <MoviePoster poster={this.props.poster} /> 
-                <h1>{this.props.title}</h1>
-            </div>
-        )
-    }
+function Movie({title, poster}) {
+    return (
+        <div>
+            <MoviePoster poster={poster} /> 
+            <h1>{title}</h1>
+        </div>
+    )
 }
 
-class MoviePoster extends Component {
-    render() {
-        return (
-            <img src={this.props.poster} />
-        )
-    }
+function MoviePoster({poster}) {
+    return (<img src={poster} />)
+    
+}
+
+Movie.prototypes = {
+    title: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired
+}
+
+MoviePoster.prototypes = {
+    poster: PropTypes.string.isRequired
 }
 
 export default Movie
